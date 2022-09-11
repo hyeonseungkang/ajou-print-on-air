5432d7163513c700bcabdd010aae53bc2ba40368
Hyeonseung Kang <h5k@ajou.ac.kr>
2022-09-11 19:37:33 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,33 +1,37 @@
-echo ""
-echo "========== Homebrew 설치 =========="
-echo ""
+ohai() {
+  printf "${tty_blue}==>${tty_bold} %s${tty_reset}\n" "$(shell_join "$@")"
+}
+
+ohai ""
+ohai "========== Homebrew 설치 =========="
+ohai ""
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-echo ""
-echo "========== node.js ghostscript 설치 =========="
-echo ""
+ohai ""
+ohai "========== node.js ghostscript 설치 =========="
+ohai ""
 brew install node ghostscript
 
-echo ""
-echo "========== pm2 설치 =========="
-echo ""
+ohai ""
+ohai "========== pm2 설치 =========="
+ohai ""
 npm install -g pm2
 
-echo ""
-echo "========== git 설치 확인 =========="
-echo ""
+ohai ""
+ohai "========== git 설치 확인 =========="
+ohai ""
 git
 
-echo ""
-echo "========== ajou-print-on-air 프로젝트 복제 =========="
-echo ""
+ohai ""
+ohai "========== ajou-print-on-air 프로젝트 복제 =========="
+ohai ""
 git clone https://github.com/khskeb0513/ajou-print-on-air
 
-echo ""
-echo "========== ajou-print-on-air 프로젝트 실행 =========="
-echo ""
+ohai ""
+ohai "========== ajou-print-on-air 프로젝트 실행 =========="
+ohai ""
 cd ajou-print-on-air
 pm2 start
 
-echo ""
-echo "** 스크립트 끝 **"
+ohai ""
+ohai "** 스크립트 끝 **"
