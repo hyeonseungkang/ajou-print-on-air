ab2b4afd334abc19ccf776adc2b5ce939c15715e
Hyeonseung Kang <h5k@ajou.ac.kr>
2022-09-11 19:44:35 +0900

    edit install.sh

--- install.sh
+++ install.sh
@@ -14,42 +14,35 @@ abort() {
 # Fail fast with a concise message when not using bash
 # Single brackets are needed here for POSIX compatibility
 # shellcheck disable=SC2292
-if [ -z "${BASH_VERSION:-}" ]
-then
+if [ -z "${BASH_VERSION:-}" ]; then
   abort "Bash is required to interpret this script."
 fi
 
 # Check if script is run with force-interactive mode in CI
-if [[ -n "${CI-}" && -n "${INTERACTIVE-}" ]]
-then
+if [[ -n "${CI-}" && -n "${INTERACTIVE-}" ]]; then
   abort "Cannot run force-interactive mode in CI."
 fi
 
 # Check if both `INTERACTIVE` and `NONINTERACTIVE` are set
 # Always use single-quoted strings with `exp` expressions
 # shellcheck disable=SC2016
-if [[ -n "${INTERACTIVE-}" && -n "${NONINTERACTIVE-}" ]]
-then
+if [[ -n "${INTERACTIVE-}" && -n "${NONINTERACTIVE-}" ]]; then
   abort 'Both `$INTERACTIVE` and `$NONINTERACTIVE` are set. Please unset at least one variable and try again.'
 fi
 
 # Check if script is run in POSIX mode
-if [[ -n "${POSIXLY_CORRECT+1}" ]]
-then
+if [[ -n "${POSIXLY_CORRECT+1}" ]]; then
   abort 'Bash must not run in POSIX mode. Please unset POSIXLY_CORRECT and try again.'
 fi
 
 # string formatters
-if [[ -t 1 ]]
-then
+if [[ -t 1 ]]; then
   tty_escape() { printf "\033[%sm" "$1"; }
 else
   tty_escape() { :; }
 fi
 tty_mkbold() { tty_escape "1;$1"; }
-tty_underline="$(tty_escape "4;39")"
 tty_blue="$(tty_mkbold 34)"
-tty_red="$(tty_mkbold 31)"
 tty_bold="$(tty_mkbold 39)"
 tty_reset="$(tty_escape 0)"
 
@@ -57,8 +50,7 @@ shell_join() {
   local arg
   printf "%s" "$1"
   shift
-  for arg in "$@"
-  do
+  for arg in "$@"; do
     printf " "
     printf "%s" "${arg// /\ }"
   done
@@ -100,7 +92,7 @@ git clone https://github.com/khskeb0513/ajou-print-on-air
 ohai ""
 ohai "========== ajou-print-on-air 프로젝트 실행 =========="
 ohai ""
-cd ajou-print-on-air
+cd ajou-print-on-air || exit
 pm2 start
 
 ohai ""
