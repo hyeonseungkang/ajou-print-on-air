11fb6c204f955c91efbbf4c4261d462fbacde107
Hyeonseung Kang <h5k@ajou.ac.kr>
2022-09-11 19:38:59 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,17 @@
+tty_blue="$(tty_mkbold 34)"
+tty_bold="$(tty_mkbold 39)"
+tty_reset="$(tty_escape 0)"
+shell_join() {
+  local arg
+  printf "%s" "$1"
+  shift
+  for arg in "$@"
+  do
+    printf " "
+    printf "%s" "${arg// /\ }"
+  done
+}
+
 ohai() {
   printf "${tty_blue}==>${tty_bold} %s${tty_reset}\n" "$(shell_join "$@")"
 }
